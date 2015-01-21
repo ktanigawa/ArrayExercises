dfea367f024e4eb3e5324be431138ab5a53486f9
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-01-20 18:13:25 -1000

    Arrays built

--- build_arrays.js
+++ build_arrays.js
@@ -3,54 +3,54 @@
  **/
 
 // Create an empty array.
-
+var emptyArray = [];
 
 // Create an array of the colors of the rainbow.
-
+var colorsRainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 // Create an array of gems.
-
+var gems = ["quartz", "hematitte", "jade", "ruby"];
 
 // Create an array of military ranks.
-
+var militaryRanks = ["Private", "Private First Class", "Lance Corporal", "Corporal", "Sergeant", "Staff Sergeant", "Gunnery Sergeant", "Master Sergeant", "First Sergeant", "Master Gunnery Sergeant", "Sergeant Major"];
 
 // Create an array of road signs.
-
+var roadSigns = ["Haleiwa Road", "Waialua Road", "Palama Road", "Manoa Road", "Punahou Street"];
 
 // Create an array containing components of a tree.
-
+var componentsTree = ["branch", "leaves", "trunk", "roots", "pith", "Medullary rays", "Soil", "Heartwood", "Sapwood"];
 
 // Create an array of prime numbers less than 20.
-
+var primeNumTwenty = [ 2, 3, 5, 7, 11, 13, 17, 19];
 
 // Create an array of words programmers typically say while programming.
-
+var fightingWords = ["objects", "arrays", "strings", "sudu", "I am root", "access"];
 
 // Create an array of elements of the periodic table.
-
+var periodicElements = ["Hydrogen", "Helium", "Lithium", "Beryllium"];
 
 // Create an array representing the coins currently in your pocket.
-
+var coinsPocket = ["Quarter", "Penny", "Nickel", "Dime", "Half Dolla\'"];
 
 // Create an array of items you bring to class daily.
-
+var itemClassDaily = ["Blue Pen", "Notebook", "Laptop", "Mousepad", "Water", "Coffee"];
 
 // Create an array of web developer role titles.
-
+var webDevTitles = ["Creative Director", "UX Designer", "UI Designer", "UI Developer", "Javascript Developer", "Backend Developer", "DBA", "System Administrator", "Dev Ops"];
 
 // Create an array of good luck charms.
-
+var goodLuckCharms = ["Rabbits' Foot", "Four-leaf Clover", "Daruma", "Happy Neko"];
 
 // Create an array of mythical animals.
-
+var mythicalAnimals = ["Narwhal", "Unicorn", "Centaur", "Sphinx", "Dragons"];
 
 // Create an array of drinks.
-
+var drinks = ["Espresso", "Latte", "Green Tea"];
 
 // Create an array of emoticons.
-
+var emoticons = ["(beer)", "(celeryman)", "(boom)", "(chewie)"];
 
 // Create an array with the letters of the word 'array'.
-
+var arrayLetters = ['a', 'r', 'r', 'a', 'y'];
 
 
